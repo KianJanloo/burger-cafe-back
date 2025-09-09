db79eda9b71195b588008285feac9d55c2593a75
Kian Janloo <ddrdslayer@gmail.com>
2025-09-09 08:55:46 +0330

    main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
+// Crypto polyfill must be at the very top before any other imports
+import { webcrypto } from 'crypto';
+if (!globalThis.crypto) {
+  globalThis.crypto = webcrypto as Crypto;
+}
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
-import { webcrypto } from 'crypto';
-
-if (!globalThis.crypto) {
-  globalThis.crypto = webcrypto as Crypto;
-}
 
 dotenv.config();
 
